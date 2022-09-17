4e44ac0f090455e4406c504de1b9f59eb5bf8ede
Nikita Chertkov <69633289+gemenee@users.noreply.github.com>
2022-09-17 20:05:29 +0300

    Добавил проверку доступности товара

--- CleverWashWebApiTestTask/Model/SalesPoint.cs
+++ CleverWashWebApiTestTask/Model/SalesPoint.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CleverWashWebApiTestTask.Model
 {
@@ -8,5 +9,10 @@ namespace CleverWashWebApiTestTask.Model
 		public string Name { get; set; }
 		public IEnumerable<ProvidedProducts> ProvidedProducts { get; set; }
 		public IEnumerable<Sale> Sales { get; set; }
+
+		public bool IsAvailable(Product product, int quantity)
+		{
+			return ProvidedProducts.Any(pp => pp.ProductId == product.Id && pp.ProductQuantity >= quantity);
+		}
 	}
 }
